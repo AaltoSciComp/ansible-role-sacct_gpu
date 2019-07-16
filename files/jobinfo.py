f9367c59c7f5c5cf2f4644483cf39d33d50d972c
Simo Tuomisto <simo.tuomisto@aalto.fi>
2019-07-16 11:40:09 +0300

    files/jobinfo.py: Fixed a bug where small jobs would not have a key in the stats json. Did some refactoring for better lint results.

--- files/jobinfo.py
+++ files/jobinfo.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
+#
+# This script loads the gpu stats json and prints the output
+#
 
+import sys
 import json
 
 def getstats(fname):
-    with open(fname) as f:
-        return json.load(f)
+    with open(fname) as stats_file:
+        return json.load(stats_file)
 
 if __name__ == '__main__':
-    import sys
-    j = getstats(sys.argv[1])
-    id = sys.argv[2]
-    del j[id]['step']
-    print(json.dumps(j[id]))
+    gpu_stats = getstats(sys.argv[1])
+    job_id = sys.argv[2]
+    if job_id in gpu_stats:
+        del gpu_stats[job_id]['step']
+        print(json.dumps(gpu_stats[job_id]))
+    else:
+        print('No GPU stats found.')
+
