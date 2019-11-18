0522c344d12aa5370703bb3277f10532977e6a81
Janne Blomqvist <janne.blomqvist@aalto.fi>
2019-11-18 19:38:47 +0200

    Update jobid command

--- files/epilog.sh
+++ files/epilog.sh
@@ -1,4 +1,4 @@
 if nvidia-smi -L|grep -q GPU; then
-  /usr/bin/scontrol update jobid=$jobid comment="$(/usr/local/bin/jobinfo.py /run/gpustats.json ${SLURM_JOB_ID})"
+  /usr/bin/scontrol update jobid=${SLURM_JOB_ID} comment="$(/usr/local/bin/jobinfo.py /run/gpustats.json ${SLURM_JOB_ID})"
 fi
 
